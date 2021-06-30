8b8aec7ef3f8ac2f6596b9be9cdb237735e09dcf
karolinaq20 <karolinaq20@hotmail.com>
2021-06-30 15:46:24 -0300

    Modificação do dia 30/06/2021

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/Repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package org.generation.blogPessoal.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario> findByUsuario(String usuario);
+	List<Usuario> findAllByUsuarioContainingIgnoreCase(String usuario);
+}
